d54f490b38e9faef24ff224a3ab11ddd8af0b8ed
Fahmi-HI <fahmiiqtishom12@gmail.com>
2026-04-06 17:47:05 +0700

    add handschecker (high, pair, 2pair)

---
+++ TwoPair.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "TwoPair.h"
+
+// dummy helper
+bool isTwoPair(const Hand& hand){
+return hand.value == 3;
+}
+HandRank TwoPairChecker::check(const Hand& hand){
+if (isTwoPair(hand)){
+std::cout << "Detected TWO PAIR\n";
+return HandRank::TWO_PAIR;
+}
+if (nextChecker)
+return nextChecker->check(hand);
+return HandRank::HIGH_CARD;
+}
